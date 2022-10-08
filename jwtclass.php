dd02ae5996948f433edc85c38ed9e012d2938820
Guilherme Andrade <42524262+TheFlaamer@users.noreply.github.com>
2022-10-08 19:12:29 -0300

    Update jwtclass.php

--- jwtclass.php
+++ jwtclass.php
@@ -1,7 +1,15 @@
 <?php
+require_once "connection.php";
+
 class MyJWT {
     private $senha = "SenhaSecreta";
-    private $expTime = 100000000;
+    private $conn = null;
+
+    function __construct() {
+        $connection = new Conn();
+        $this->conn = $connection->connect();
+    }
+
     public function criaToken($payload){
         $header = [
             'alg' => 'SHA256',
@@ -11,12 +19,6 @@ class MyJWT {
          $header = json_encode($header);
          $header = base64_encode($header);
 
-         $exp = $this->expTime + time();
-
-         array_push($payload, $exp);
-
-         echo $payload;
-
          $payload = json_encode($payload);
          $payload = base64_encode($payload);
         
@@ -25,7 +27,7 @@ class MyJWT {
         
          return "$header.$payload.$signature";
     }
-    public function validaToken($jwt){
+    public function validaToken($jwt, $expires){
          $part = explode(".",$jwt);
          $header = $part[0];
          $payload = $part[1];
@@ -34,7 +36,18 @@ class MyJWT {
          $signatureCheck = hash_hmac('sha256',"$header.$payload",$this->senha,true);
          $signatureCheck = base64_encode($signatureCheck);
          if ($signature == $signatureCheck){
-            $retorno = true;
+            if($expires == true) {
+                $payload = base64_decode($payload);
+                $payload = json_decode($payload, true);
+         
+                 if($payload['exp'] >= time()) {
+                    $retorno = true;
+                 } else {
+                    $retorno = false;
+                 }
+            } else {
+                $retorno = true;
+            }
          }else {
             $retorno = false;
          }
@@ -42,8 +55,27 @@ class MyJWT {
          return $retorno;
     }
 
-    public function refreshToken($token) {
+    public function extractToken($token, $pos) {
+      $token_extracted = explode(".",$token);
+      $payloadData = base64_decode($token_extracted[$pos]);
+      $payloadData = json_decode($payloadData, true);
+      print_r($payloadData);
 
+      return $payloadData;
+
+      // $signatureCheck = hash_hmac('sha256',"$header.$payload",$this->senha,true);
+      // $signatureCheck = base64_encode($signatureCheck);
+    }
+
+    public function getBlacklist($token) {
+      $q = "SELECT * FROM token WHERE refresh_token = '".$token."'";
+      $sql = mysqli_query($this->conn, $sql);
+      return $sql->num_rows;
+    }
+    public function insertBlackList($hash, $id) {
+        $q = "INSERT INTO token (refresh_token, id_user) VALUES ('$hash', $id)";
+        $sql = mysqli_query($this->conn, $q);
+        return $sql;
     }
 }
 ?>
